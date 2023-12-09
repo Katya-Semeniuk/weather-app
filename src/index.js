02c0c61ed99c9beb72a0500f2604db755eba3ce5
Katya-Semeniuk <katyasemeniuk@gmail.com>
2023-12-09 10:58:04 +0100

    added links

--- src/index.js
+++ src/index.js
@@ -1,45 +1,99 @@
-const apiKey = "8907b2tf0b29bfcd0b41134b5b1c6ao9";
-const API = "https://api.shecodes.io/weather/v1/current?";
+// const apiKey = "8907b2tf0b29bfcd0b41134b5b1c6ao9";
+// const API = "https://api.shecodes.io/weather/v1/current?";
 
-let searchInputElement = document.querySelector("#search-input");
-let cityElement = document.querySelector("#current-city");
-let descriptionEl = document.getElementById("description");
-let windEl = document.getElementById("wind");
-let humidityEl = document.getElementById("humidity");
-let temperatureValueEl = document.querySelector(".current-temperature-value");
+// let searchInputElement = document.querySelector("#search-input");
+// let cityElement = document.querySelector("#current-city");
+// let descriptionEl = document.getElementById("description");
+// let windEl = document.getElementById("wind");
+// let humidityEl = document.getElementById("humidity");
+// let temperatureValueEl = document.querySelector(".current-temperature-value");
+
+// function search(event) {
+//   event.preventDefault();
+//   let query = searchInputElement.value;
+
+//   axios
+//     .get(API, {
+//       params: {
+//         query: query,
+//         key: apiKey,
+//       },
+//     })
+//     .then(getCityInfo)
+//     .catch(function (error) {
+//       console.log(error);
+//     })
+//     .finally(function () {});
+// }
+
+// function getCityInfo(res) {
+//   const data = res.data;
+//   const city = data.city;
+//   const temperature = Math.round(data.temperature.current);
+//   const description = data.condition.description;
+//   let wind = data.wind.speed;
+//   let humidity = data.temperature.humidity;
+
+//   cityElement.innerHTML = city;
+//   descriptionEl.innerHTML = description;
+//   windEl.innerHTML = wind;
+//   humidityEl.innerHTML = humidity;
+//   temperatureValueEl.innerHTML = temperature;
+// }
+// function formatDate(date) {
+//   let minutes = date.getMinutes();
+//   let hours = date.getHours();
+//   let day = date.getDay();
+
+//   if (minutes < 10) {
+//     minutes = `0${minutes}`;
+//   }
+
+//   if (hours < 10) {
+//     hours = `0${hours}`;
+//   }
+
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+
+//   let formattedDay = days[day];
+//   return `${formattedDay} ${hours}:${minutes}`;
+// }
+
+// let searchForm = document.querySelector("#search-form");
+// searchForm.addEventListener("submit", search);
+
+// let currentDateELement = document.querySelector("#current-date");
+// let currentDate = new Date();
+
+// currentDateELement.innerHTML = formatDate(currentDate);
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
 
 function search(event) {
   event.preventDefault();
-  let query = searchInputElement.value;
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
 
-  axios
-    .get(API, {
-      params: {
-        query: query,
-        key: apiKey,
-      },
-    })
-    .then(getCityInfo)
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {});
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function getCityInfo(res) {
-  const data = res.data;
-  const city = data.city;
-  const temperature = Math.round(data.temperature.current);
-  const description = data.condition.description;
-  let wind = data.wind.speed;
-  let humidity = data.temperature.humidity;
-
-  cityElement.innerHTML = city;
-  descriptionEl.innerHTML = description;
-  windEl.innerHTML = wind;
-  humidityEl.innerHTML = humidity;
-  temperatureValueEl.innerHTML = temperature;
-}
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
